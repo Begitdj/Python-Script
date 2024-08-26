14656637422e075da3955b2731205771745ec96e
Begitdj <egyptha29@gmail.com>
2024-08-26 17:05:22 +0700

    Add files via upload

---
+++ FolderCreate.py
@@ -0,0 +1,4 @@
+import os
+print("Folder Creator")
+test = input("Enter path to create folder Example:/folder/Folder1 this example create in folder with name folder folder with name Folder1:")
+os.mkdir(test)
