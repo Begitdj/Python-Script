83141e97a895c859365ccdab203730cb4d8bf23b
Begitdj <egyptha29@gmail.com>
2024-08-26 17:49:03 +0700

    Update FolderCreate.py
    Added check folder existing

--- FolderCreate.py
+++ FolderCreate.py
@@ -1,4 +1,5 @@
 import os
 print("Folder Creator")
 test = input("Enter path to create folder Example:/folder/Folder1 this example create in folder with name folder folder with name Folder1:")
-os.mkdir(test)
+if not os.path.exists(test):
+	os.mkdir(test)
